cc585c3da33b120eb2e43bab635f8616ae483c77
repl.it user <replituser@example.com>
2020-12-29 07:35:39 +0000

    Updates

---
+++ precache-manifest.44916f0472cd77b8fd52b03e6fe7a027.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd1edac90113a88087029a5cde5e3c2d",
+    "url": "/git-issues-page/index.html"
+  },
+  {
+    "revision": "29d7ce4da7f1e15d4d14",
+    "url": "/git-issues-page/static/css/main.9b1431d8.chunk.css"
+  },
+  {
+    "revision": "4f17f23b3527441c32b6",
+    "url": "/git-issues-page/static/js/2.f2fd6f87.chunk.js"
+  },
+  {
+    "revision": "29d7ce4da7f1e15d4d14",
+    "url": "/git-issues-page/static/js/main.0583e1d5.chunk.js"
+  },
+  {
+    "revision": "ed1375c5baafcd6099f9",
+    "url": "/git-issues-page/static/js/runtime~main.87d144c0.js"
+  }
+]);
